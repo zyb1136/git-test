58edee5f5119a1b4b631bd93b9af3e3504bbd9c5
Zhang <2059086013@qq.com>
2023-08-27 16:43:09 +0800

    first commit

---
+++ src/main/java/com/zhang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zhang.git;
+
+/**
+ * @author 张一波
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
